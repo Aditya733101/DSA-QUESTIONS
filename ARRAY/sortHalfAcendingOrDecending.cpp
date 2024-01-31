0c9ecfa6d687d3797a02ea89fdfc1a694ca7f636
ADITYA SARKAR <104191788+Aditya733101@users.noreply.github.com>
2024-01-31 11:31:04 +0000

    Sort first half in ascending order and second half in descending

---
+++ ARRAY/sortHalfAcendingOrDecending.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int arr[] ={1,2,3,4,5,6};
+  int size =sizeof(arr)/sizeof(arr[0]);
+  for(int i=0;i<size/2;i++)
+  {
+    cout<<arr[i]<<" ";
+  }
+  for(int i=size-1;i>=size/2;i--)
+  {
+    cout<<arr[i]<<" ";
+  }
+}
